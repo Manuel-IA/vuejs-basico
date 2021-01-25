0e0affe251c432c71f75d1e6857d6b3c97610af0
Manuel-IA <manuelpc1720@gmail.com>
2021-01-25 10:44:57 -0500

    first commit

---
+++ src/router.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/views/Home";
+import CoinDetail from "@/views/CoinDetail";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/coin/:id",
+      name: "coin-detail",
+      component: CoinDetail
+    }
+  ]
+});
